c88eeaf8b956bbee3c8cafd1dd5b1e636143f36f
Jack Huang <f26401004@gmail.com>
2017-10-04 16:59:06 +0800

    alert test.

--- js/background_music.js
+++ js/background_music.js
@@ -137,7 +137,7 @@ function Visualizer() {
                 ctx.fillStyle = gradient;
                 ctx.fillRect(i * gap + 1, cheight - value + capHeight + 1, meterWidth - 2, cheight - 2);
             }
-            average = average * 640 / step;
+            average = average / step;
             alert(average);
             $('#photo').css({
                 width: String(average) + '%',
