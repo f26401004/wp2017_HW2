faf5a2aa8fb0a40812c8ed2d447881b64ca2381b
Jack Huang <f26401004@gmail.com>
2017-10-04 15:08:48 +0800

    change bar style.

--- js/background_music.js
+++ js/background_music.js
@@ -93,9 +93,8 @@ function Visualizer() {
         // 獲取 canvas 內容繪製
         ctx = canvas.getContext('2d'),
             gradient = ctx.createLinearGradient(0, 0, 0, 300);
-        gradient.addColorStop(0, '#ca2fa7');
-        gradient.addColorStop(0.1, '#ff5151');
-        gradient.addColorStop(0.1, '#ff5151');
+        gradient.addColorStop(0.9, '#ca2fa7');
+        gradient.addColorStop(1, '#ff5151');
         var drawMeter = function () {
             var array = new Uint8Array(analyser.frequencyBinCount);
             analyser.getByteFrequencyData(array);
